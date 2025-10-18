65da8d42a28c3b857c319ff04ed91e53bbc84b07
eguilbert <eguilbert@jalons.com>
2025-10-18 21:56:50 +0200

    fix: correct login redirect

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,10 +1,13 @@
 // middleware/auth.js
-export default defineNuxtRouteMiddleware(async () => {
-  const { isAuthenticated, ensureUserLoaded } = useAuth();
+export default defineNuxtRouteMiddleware(async (to, from) => {
+  const auth = useAuth(); // récupérer ton composable
+  const { isAuthenticated, ensureUserLoaded } = auth;
 
+  // ⚙️ S'assurer que le user est bien chargé avant de décider
   await ensureUserLoaded();
 
+  // 🔒 Si non connecté → redirection login
   if (!isAuthenticated.value) {
-    return navigateTo("/login");
+    return navigateTo(`/login?redirect=${to.fullPath}`);
   }
 });
