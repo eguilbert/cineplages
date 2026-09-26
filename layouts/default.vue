5aba0032def672d02dadf027c118b6361781b146
eguilbert <eguilbert@jalons.com>
2026-09-26 15:27:03 +0200

    Add recommendation administration page

--- layouts/default.vue
+++ layouts/default.vue
@@ -266,6 +266,7 @@ const navGroups = computed(() => [
       { label: "Tags", to: "/films/TagValidation" },
       { label: "Film Tags", to: "/films/tags" },
       { label: "Admin", to: "/admin" },
+      { label: "Recommandations", to: "/admin/recommendations" },
     ],
   },
 ]);
